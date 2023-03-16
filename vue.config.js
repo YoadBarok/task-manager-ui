da041a54a6635939bfeb7c984320efb02e20abbf
Yoad Barok <yoadbarok@hotmail.com>
2023-03-16 13:37:53 +0100

    enable treeshaking

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const { DefinePlugin } = require('webpack');
+const { VuetifyPlugin } = require('webpack-plugin-vuetify')
+
+
+module.exports = {
   transpileDependencies: true,
 
+  configureWebpack: {
+    plugins: [
+      new DefinePlugin({
+        'process.env.VUETIFY_USE_TREE_SHAKE': true
+      })
+    ]
+  },
+
   pluginOptions: {
+    plugins: [new VuetifyPlugin()],
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+    }
   }
-})
+};
